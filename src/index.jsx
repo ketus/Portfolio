4fa2c53a3521dcd09a21f0eab48d5e7c9e8790ea
ketus <bardzo.mlody@gmail.com>
2017-04-27 17:51:55 +0200

    working skeleton

---
+++ src/index.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+class Layout extends Component {
+  render(){
+    return <h1>wat</h1>
+  }
+}
+
+ReactDOM.render(
+  <Layout/>,
+  document.getElementById('app')
+)
