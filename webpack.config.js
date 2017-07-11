6e1930e21f0ccfe34231b36290276bbb0c9b3e65
ketus <bardzo.mlody@gmail.com>
2017-07-11 10:26:17 +0200

    added redirects that netlify requires

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
     }],
   },
   output: {
-    path: path.join(__dirname, 'build'),
+    path: path.join(__dirname, 'public'),
     filename: 'client.min.js',
     publicPath: '/',
   },
